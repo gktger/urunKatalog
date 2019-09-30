46e7f91fbf25947af68174cc85f0324d4b8c2238
Goktug <goktuger@hotmail.com>
2019-09-30 10:12:28 +0300

    Sepet Olusturuldu.

---
+++ urunKatalog.MvcWebUI/Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using urunKatalog.MvcWebUI.Entity;
+using urunKatalog.MvcWebUI.Models;
+
+namespace urunKatalog.MvcWebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private  DataContext db= new DataContext();
+
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+
+        public ActionResult AddToCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+            if (product!=null)
+            {
+                GetCart().AddProduct(product,1);
+            }
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult RemoveToCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            var cart = (Cart) Session["Cart"];
+            if (cart==null)
+            {
+                cart=new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+
+        
+    }
+}
