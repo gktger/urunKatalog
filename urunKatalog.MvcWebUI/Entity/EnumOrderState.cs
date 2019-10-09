86e45424cb7d7f6436580cd8040ba376c2346004
Goktug <goktuger@hotmail.com>
2019-10-09 23:14:54 +0300

    admin paneline sipariş onayı eklendi

--- urunKatalog.MvcWebUI/Entity/EnumOrderState.cs
+++ urunKatalog.MvcWebUI/Entity/EnumOrderState.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +9,10 @@ namespace urunKatalog.MvcWebUI.Entity
 {
     public enum EnumOrderState
     {
+
+        [Display(Name = "Onay Bekleniyor")]
         Waiting,
+        [Display(Name = "Kargolandı")]
         Completed
     }
 }
